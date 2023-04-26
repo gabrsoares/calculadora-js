2b6fbefc4404a5472368dbd3b68119bebbb6059a
Gabriel Soares <gabrsoares@outlook.com>
2023-04-26 16:30:28 -0300

    correcao codigo & finalizando projeto

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let numeroBotao = document.querySelectorAll('.number')
 let operacaoBotao = document.querySelectorAll('.operation')
 let display = document.getElementById('visor-display')
+let displayMenor = document.getElementById('previous-display')
 let igual = document.getElementById('equal')
 let operando1 = ""
 let operando2 = ""
@@ -10,26 +11,24 @@ let resultado
 
 document.getElementById('button-clear').addEventListener('click', () => { // limpa a tela ao apertar o "C"
     display.innerHTML = ""
+    displayMenor.innerHTML = ""
     operando1 = ""
     operando2 = ""
     operacao = ""
 })
 
+let maximo = screen.width < 500 ? 12 : 17 // usado pra definir o tamanho maximo do visor de acordo com o tamanho da tela
+
 numeroBotao.forEach(item => {
     item.addEventListener('click', (e) => {
-        if(display.innerHTML.length >= 18){ // to tentando limitar apenas o operando1 mas nao consigo
-            e.preventDefault
-        } else {
-            if (operando1 === "") { // limpa a tela após apertar o = e digitar de novo (operando1="" lá na função resultado)
-            display.innerHTML = ""
-            }
-            if (operacao === "") {
-                operando1 += item.innerHTML.replace(/ /g, "")
-                
+        if(display.innerHTML.length >= maximo){
+            if(operando1 != ""){
+                e.preventDefault
             } else {
-                operando2 += item.innerHTML.replace(/ /g, "")
+                atribuirNumero(item.innerHTML)
             }
-            display.innerHTML += item.innerHTML.replace(/ /g, "")
+        } else {
+            atribuirNumero(item.innerHTML)
         }
     })
 })
@@ -40,7 +39,8 @@ operacaoBotao.forEach(item => {
             e.preventDefault
         } else {
             operacao = item.innerHTML
-            display.innerHTML += item.innerHTML.replace(/ /g, "")
+            display.innerHTML = item.innerHTML + " "
+            displayMenor.innerHTML = operando1.replace(/ /g, "")
         }
     })
 })
@@ -48,6 +48,7 @@ operacaoBotao.forEach(item => {
 igual.addEventListener('click', () => {
     if (operando1 === "" && operacao === "" && operando2 === ""){
         display.innerHTML = "0"
+        displayMenor.innerHTML = ""
     } else {
         if(operacao === "" && operando2 === ""){
             contaMat = operando1
@@ -57,6 +58,7 @@ igual.addEventListener('click', () => {
             contaMat = operando1 + operacao + operando2
         }
         resultado = Function("return " + contaMat)();
+        displayMenor.innerHTML = ""
         display.innerHTML = resultado
         operando1 = "" // necessário para limpar a tela quando digitar de novo
         operacao = ""
@@ -65,3 +67,69 @@ igual.addEventListener('click', () => {
     
 })
 
+document.addEventListener('keypress', (e) => { 
+    const valorTecla = e.key // armazena o valor da tecla
+    
+    if(valorTecla.match("^[0-9]")){ // verifica se é numero
+        if(display.innerHTML.length >= maximo){
+            if(operando1 != ""){
+                e.preventDefault
+            } else {
+                atribuirNumero(valorTecla)
+            }
+        } else {
+            atribuirNumero(valorTecla)
+        }
+    
+    } else if (valorTecla.match("[-+*/]")) { //verifica se é operação
+        
+        if (operando1 === "" || operacao !== "") {
+            
+            e.preventDefault
+            
+        } else {
+            
+            operacao = valorTecla
+            displayMenor.innerHTML = operando1.replace(/ /g, "")
+            display.innerHTML = operacao
+            
+        }
+    } else if (valorTecla === "=" || valorTecla === "Enter") {
+        
+        if (operando1 === "" && operacao === "" && operando2 === ""){
+            
+            display.innerHTML = "0"
+            
+        } else {
+            
+            if(operacao === "" && operando2 === ""){
+                contaMat = operando1
+            } else if (operacao !== "" && operando2 === ""){
+                contaMat = operando1 + operacao + operando1
+            } else {
+                contaMat = operando1 + operacao + operando2
+            }
+            resultado = Function("return " + contaMat)(); // calcula o valor da string sem usar eval
+            display.innerHTML = resultado
+            displayMenor.innerHTML = ""
+            operando1 = "" // necessário para limpar a tela quando digitar de novo
+            operacao = ""
+            operando2 = ""
+            contaMat = ""
+        }
+    }
+})
+
+function atribuirNumero(numero) {
+    if (operando1 === "") { // limpa a tela após apertar o = e digitar de novo (operando1 = "" lá na função resultado)
+        display.innerHTML = ""
+    }
+    if (operacao === "") {
+        operando1 += numero.replace(/ /g, "")
+        display.innerHTML += numero.replace(/ /g, "")
+        
+    } else {
+        operando2 += numero.replace(/ /g, "")
+        display.innerHTML += numero.replace(/ /g, "")
+    }
+}
